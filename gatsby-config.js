584059046c25c43c952e0d96e509824e436d0b46
Alessandro Di Tecco <alessandro@ditecco.me>
2021-01-06 17:07:10 +0100

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,15 +6,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //   },
+    // },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    { resolve: 'gatsby-plugin-sharp', options: { failOnError: false } },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,6 +25,9 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // { resolve: "@chakra-ui/gatsby-plugin",
+    //   options: {}
+    // }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
