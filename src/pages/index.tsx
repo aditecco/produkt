a4961fe7f4bbfe3b4858ff698d6ea5141a833285
Alessandro Di Tecco <alessandro@ditecco.me>
2021-01-06 20:34:29 +0100

    Homepage wrap WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 Home (index.js)
 --------------------------------- */
 
-import React from "react"
+import * as React from "react"
 import {
   Box,
   Text,
@@ -11,9 +11,19 @@ import {
   Flex,
   Spacer,
   Container,
+  AspectRatio,
+  Heading,
+  SimpleGrid,
+  Link,
 } from "@chakra-ui/react"
 
+import { Stack, HStack, VStack } from "@chakra-ui/react"
+
 import styled from "@emotion/styled"
+
+/**
+ * TODO move to own components
+ */
 
 const Circle = styled.span`
   display: inline-block;
@@ -23,23 +33,35 @@ const Circle = styled.span`
   background-color: rebeccapurple;
 `
 
-const Section: React.FC = ({ children }) => {
+const Section: React.FC = ({ children, cWidth, ...props }) => {
   return (
-    <Box as={"section"}>
-      <Container>{children}</Container>
+    <Box as={"section"} {...props}>
+      <Container
+        centerContent
+        maxWidth={cWidth ?? "container.lg"}
+        h={"full"}
+        justifyContent={"center"}
+      >
+        {children}
+      </Container>
     </Box>
   )
 }
 
+/**
+ *
+ * HOME
+ *
+ */
 export default () => (
   <>
-    <Box as={"nav"} bg={"gray.50"}>
+    <Box as={"nav"} bg={"gray.100"}>
       {/*TODO*/}
-      <Container maxW={"960px"}>
+      <Container maxWidth={"container.lg"}>
         <Flex justify={"space-between"} align={"center"} py={3}>
           <Box>
             {/*<Image src={"#"} />*/}
-            Logo
+            Produkt
           </Box>
 
           <Spacer />
@@ -51,6 +73,98 @@ export default () => (
       </Container>
     </Box>
 
-    <Section>yo</Section>
+    {/*
+      SECTION
+    */}
+
+    <Section h={400} bg={"gray.50"} cWidth={"container.md"}>
+      <Heading>Produkt</Heading>
+    </Section>
+
+    {/*
+      SECTION
+    */}
+
+    <Section>
+      <SimpleGrid columns={[1, 3]} spacing={5} w={"full"} py={16}>
+        <Box p={5} shadow="md">
+          <Heading fontSize="xl">Item 1</Heading>
+          <Text mt={4}>alta muta Cur nixus congregabo?</Text>
+        </Box>
+
+        <Box p={5} shadow="md">
+          <Heading fontSize="xl">Item 2</Heading>
+          <Text mt={4}>Accentor de clemens brodium, imperium acipenser!</Text>
+        </Box>
+
+        <Box p={5} shadow="md">
+          <Heading fontSize="xl">Item 3</Heading>
+          <Text mt={4}>Volare etiam ducunt ad camerarius abnoba.</Text>
+        </Box>
+      </SimpleGrid>
+    </Section>
+
+    {/*
+      SECTION
+    */}
+
+    <Section>
+      <Flex py={16} w={"full"} wrap={["wrap", "nowrap"]}>
+        <Box w={["full", 1 / 2]} h={[250, 500]} p={5} bg={"gray.100"} />
+
+        <Box w={["full", 1 / 2]} h={[250, 500]} p={5} bg={"gray.50"} />
+      </Flex>
+    </Section>
+
+    {/*
+      SECTION
+    */}
+
+    <Section>
+      <Flex py={16} w={"full"} wrap={["wrap", "nowrap"]}>
+        <Box w={["full", 1 / 2]} h={[250, 500]} p={5} bg={"gray.50"} />
+
+        <Box w={["full", 1 / 2]} h={[250, 500]} p={5} bg={"gray.100"} />
+      </Flex>
+    </Section>
+
+    {/*
+      SECTION
+    */}
+
+    <Section>
+      <SimpleGrid columns={2} spacing={5} w={"full"}>
+        <Box bg="gray.100" h={160} />
+        <Box bg="gray.100" h={160} />
+        <Box bg="gray.100" h={160} />
+        <Box bg="gray.100" h={160} />
+        <Box bg="gray.100" h={160} />
+        <Box bg="gray.100" h={160} />
+      </SimpleGrid>
+    </Section>
+
+    <Box as={"footer"} bg={"gray.100"} mt={5}>
+      <Container>
+        <Stack
+          spacing={4}
+          direction={["column", "row"]}
+          justify={[null, "center"]}
+          py={8}
+        >
+          <Link>Lorem</Link>
+          <Link>Ipsum</Link>
+          <Link>Dolor</Link>
+          <Link>Sit</Link>
+          <Link>Amet</Link>
+        </Stack>
+      </Container>
+    </Box>
+
+    {/*
+      SECTION
+
+    <Section>
+    </Section>
+    */}
   </>
 )
