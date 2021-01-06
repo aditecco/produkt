a44e3c9c7793036e23ae7ea5a1751289961e7530
Alessandro Di Tecco <alessandro@ditecco.me>
2021-01-06 17:08:01 +0100

    Added theme customization boilerplate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+import { ChakraProvider, CSSReset } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react"
+import themeOverrides from "./src/theme"
+
+const theme = extendTheme(themeOverrides)
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      {element}
+    </ChakraProvider>
+  )
+}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+
+// gatsby-ssr.js
+export { wrapRootElement } from "./gatsby-browser"
